3dc1f8335531bfc3d1613f8ad50202bb5a3ae39d
GacheruJr <mke445571@gmail.com>
2022-05-28 23:03:24 +0300

    just some python basics

---
+++ akan.py
@@ -0,0 +1,41 @@
+# basic python print function
+print("what to print?")
+
+####################################
+
+# string manipulation
+print("hello python\ngood days ahead")
+
+#############################################
+
+
+# string concatenation...spacing are very important
+print("hello" +" " + "maina")
+
+########################################
+
+
+#exercise python input function
+print("hello" +" "+input("what is your name?"))
+
+#######################################
+
+# excercise...calculating the length of string characters
+name = input("what is your name? ")
+length = len(name)
+print(length)
+# the lines will be executed one by one and the users input calculated it's length
+
+##########################################
+
+# python variables
+test = "jack"
+print(name)
+# the name that was input will be printed  on the terminal
+# python code executed line by line
+
+######################################
+
+# RULES OF NAMING VARIABLES
+laravel_bagisto = "php" #underscore
+laravelBagisto = "php technology" #camelcase
